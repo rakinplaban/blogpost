3e6a8e57650e216edcba0bedb6f143df6c12acb9
rakinplaban <rakinshahriar54@gmail.com>
2022-11-03 22:14:47 +0600

    learnt rendering templates.

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'title' : "Blogpost 1",
+        'author' : "Jhon Doe",
+        'date_posted' : "20 April, 2022",
+        'content' : "First Blog post" 
+    },
+    {
+        'title' : "Blogpost 2",
+        'author' : "Alice Chan",
+        'date_posted' : "25 April, 2022",
+        'content' : "Second Blog post" 
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1 style='font-size:200px;color:green;'>Hello world!</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/new_route")
 def new_route():
-    return "<h1 style='font-size:20px;color:yellow;'>Hello world!</h1>"
+    return render_template('about.html',title='About')
 
 
 if __name__ == '__main__':
