845e58385794fe918cc841be5cc136b137c55eba
rakinplaban <rakinshahriar54@gmail.com>
2022-11-13 14:15:05 +0600

    Reset password.

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,9 +4,9 @@ import os
 class Config:
     SECRET_KEY = '2a509b6cc825d66df73ca4df444ef072'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.mailtrap.io'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = '1f59c487c0cecf'
+    MAIL_PASSWORD = '5e5b5fb74a87c8'
 
